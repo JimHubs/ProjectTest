bf446bfdf7f420204560bb54ec77eb2a473a660e
jimu <mr.jpsbx@gmail.com>
2025-09-15 12:44:44 +0200

    DagligSkaev_doegnDosis0

--- Ordinationstuderende/Test/controller/ControllerTest.java
+++ Ordinationstuderende/Test/controller/ControllerTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.TestTemplate;
 import javax.swing.*;
 import javax.xml.xpath.XPath;
 import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -78,6 +80,33 @@ class ControllerTest {
         pn.givDosis(LocalDate.of(2025,3,18));
 
         assertEquals(12, pn.samletDosis());
+    }
+
+    @org.junit.jupiter.api.Test
+    void DagligSkaev_OpretDosis() {
+
+        ArrayList<Dosis> doser = new ArrayList<>();
+
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligSkaev dagligSkaev = new DagligSkaev(LocalDate.of(2025,3,14), LocalDate.of(2025,3,28), laegemiddel, doser);
+
+        dagligSkaev.opretDosis(LocalTime.of(8,0),5);
+        dagligSkaev.opretDosis(LocalTime.of(15,0), 7);
+
+        assertEquals(12, dagligSkaev.samletDosis());
+        assertEquals(0.8, dagligSkaev.doegnDosis());
+
+    }
+
+    @Test
+    void DagligSkaev_doegnDosis0(){
+        ArrayList<Dosis> doser = new ArrayList<>();
+
+        Laegemiddel laegemiddel = new Laegemiddel("Paracetamol", 1, 1.5, 2, "Ml");
+        DagligSkaev dagligSkaev = new DagligSkaev(LocalDate.of(2025,3,14), LocalDate.of(2025,3,28), laegemiddel, doser);
+
+
+        assertEquals(0, dagligSkaev.doegnDosis());
     }
 
     @org.junit.jupiter.api.Test
