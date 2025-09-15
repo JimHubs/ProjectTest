88cd86c61cc1c08b772cc5fbc508a6aac4e55e3a
MrJones2k4 <jonasmh2004@gmail.com>
2025-09-15 11:44:20 +0200

    AnbefaletDosisPrDøgn tests

--- Ordinationstuderende/Test/controller/ControllerTest.java
+++ Ordinationstuderende/Test/controller/ControllerTest.java
@@ -1,7 +1,25 @@
 package controller;
 
+import ordination.Laegemiddel;
+import ordination.Patient;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.internal.matchers.Null;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ControllerTest {
+
+    private Controller controller;
+    private Laegemiddel standardLaegemiddel;
+
+    @BeforeEach
+    void setUp(){
+        controller = Controller.getController();
+        standardLaegemiddel = new Laegemiddel(
+                "Fent", 0.1,0.15,
+                0.3,"NULL");
+    }
 
     @org.junit.jupiter.api.Test
     void getController() {
@@ -13,6 +31,7 @@ class ControllerTest {
 
     @org.junit.jupiter.api.Test
     void opretPNOrdination() {
+
     }
 
     @org.junit.jupiter.api.Test
@@ -25,10 +44,74 @@ class ControllerTest {
 
     @org.junit.jupiter.api.Test
     void ordinationPNAnvendt() {
+
     }
 
     @org.junit.jupiter.api.Test
-    void anbefaletDosisPrDoegn() {
+    void anbefaletDosisPrDoegn_NULL_Patient() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+        controller.anbefaletDosisPrDoegn(null,standardLaegemiddel);
+        });
+        assertEquals("Parametre må ikke være null", exception.getMessage());
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_NULL_Laegemiddel() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            Patient patient = new Patient("1","John", 10.0);
+            controller.anbefaletDosisPrDoegn(patient,null);
+        });
+        assertEquals("Parametre må ikke være null", exception.getMessage());
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_Let_24() {
+        Patient patient = new Patient("1","John", 24);
+        double forventetResultat = patient.getVaegt() *
+                standardLaegemiddel.getEnhedPrKgPrDoegnLet();
+        double faktiskResultat = controller.anbefaletDosisPrDoegn(patient,
+                standardLaegemiddel);
+        assertEquals(forventetResultat, faktiskResultat);
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_Normal_25() {
+        Patient patient = new Patient("1","John", 25);
+        double forventetResultat = patient.getVaegt() *
+                standardLaegemiddel.getEnhedPrKgPrDoegnNormal();
+        double faktiskResultat = controller.anbefaletDosisPrDoegn(patient,
+                standardLaegemiddel);
+        assertEquals(forventetResultat, faktiskResultat);
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_Normal_119() {
+        Patient patient = new Patient("1","John", 119);
+        double forventetResultat = patient.getVaegt() *
+                standardLaegemiddel.getEnhedPrKgPrDoegnNormal();
+        double faktiskResultat = controller.anbefaletDosisPrDoegn(patient,
+                standardLaegemiddel);
+        assertEquals(forventetResultat, faktiskResultat);
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_Normal_120() {
+        Patient patient = new Patient("1","John", 120);
+        double forventetResultat = patient.getVaegt() *
+                standardLaegemiddel.getEnhedPrKgPrDoegnNormal();
+        double faktiskResultat = controller.anbefaletDosisPrDoegn(patient,
+                standardLaegemiddel);
+        assertEquals(forventetResultat, faktiskResultat);
+    }
+
+    @org.junit.jupiter.api.Test
+    void anbefaletDosisPrDoegn_Tung_121() {
+        Patient patient = new Patient("1","John", 121);
+        double forventetResultat = patient.getVaegt() *
+                standardLaegemiddel.getEnhedPrKgPrDoegnTung();
+        double faktiskResultat = controller.anbefaletDosisPrDoegn(patient,
+                standardLaegemiddel);
+        assertEquals(forventetResultat, faktiskResultat);
     }
 
     @org.junit.jupiter.api.Test
